97f40b7b60279790a568d351993fea77f513132d
Eranga Kodithuwakku <132680494+gitEranga@users.noreply.github.com>
2024-11-19 00:42:42 +0530

    Update main.tf

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "example" {
-  ami           = "i-0cc5a298645472646" # desired AMI ID
+  ami           = "ami-040c33c6a51fd5d96" # desired AMI ID
   instance_type = var.instance_type
 
   tags = {
